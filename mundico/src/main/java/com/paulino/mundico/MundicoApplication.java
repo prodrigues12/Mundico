f98bf8f303036f8780e85e7a322551742f5de0c3
prodrigues12 <paulinorn12@gmail.com>
2023-12-20 01:02:09 -0300

    raw - alteração no pow

---
+++ mundico/src/main/java/com/paulino/mundico/MundicoApplication.java
@@ -0,0 +1,13 @@
+package com.paulino.mundico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundicoApplication.class, args);
+	}
+
+}
