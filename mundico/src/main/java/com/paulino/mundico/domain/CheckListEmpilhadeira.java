d94dce258970b3e4008d254d5a673b693191a2a8
prodrigues12 <paulinorn12@gmail.com>
2023-12-21 05:13:31 -0300

    feat:-  ChecklistEpilhadeora

---
+++ mundico/src/main/java/com/paulino/mundico/domain/CheckListEmpilhadeira.java
@@ -0,0 +1,47 @@
+package com.paulino.mundico.domain;
+
+import com.paulino.mundico.DTO.ChekListEmpDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CheckListEmpilhadeira  implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String idMagalu;
+    private int numero;
+
+    @Enumerated(EnumType.STRING)
+    private SetorType setor;
+    @Enumerated(EnumType.STRING)
+    private TurnoType turno;
+
+    private BigDecimal horimetro;
+
+    private boolean pergunta1;
+
+    private boolean status;
+
+    public CheckListEmpilhadeira (ChekListEmpDTO dto){
+        this.idMagalu= dto.idmagalu();
+        this.numero = dto.numero();
+        this.setor= dto.setorType();
+        this.turno = dto.turnoType();
+        this.horimetro=dto.horimetro();
+        this.pergunta1=dto.pergunta1();
+    }
+
+
+}
